556f22749013d517256d43b1820faab6a6b1f159
buck005 <buck005@163.com>
2016-10-23 17:34:33 +0800

    1.3

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
  console.log(window.innerHeight);//642*/
 
 window.onload = function () {
+
     var oHeader = $('header')
     var oNav = $('nav');
     var oArrow = $('arrow');
@@ -20,6 +21,7 @@ window.onload = function () {
     var oHomeContent = $('homeContent');
     var oHomeContent1 = getByClass(oHomeContent, 'homeContent1')[0];
     var oHomeContent2 = getByClass(oHomeContent, 'homeContent2')[0];
+    var oCourseContent = $('courseContent');
 
     var oMenu_box = $('menu_box');
     var aLiMenu = oMenu_box.getElementsByTagName('li');
@@ -31,17 +33,76 @@ window.onload = function () {
     var oAside = $('aside');
     var aLiAside = oAside.getElementsByTagName('li');
 
+    var oAuthorContent = $('authorContent');
+    var oAuthorContent1 = getByClass(oAuthorContent, 'authorContent1')[0];
+    var oAuthorContent2 = getByClass(oAuthorContent, 'authorContent2')[0];
+    var oMusic = $('music');
+    var oAudio1 = $('audio1');
+    var oLoading = $('loading');
+
     var iNow = 0;
+    var prevIndex = 0;
     var bOk = true;
 
+    showLoading();
     contentAuto();
     listContentAuto();
     bindNav();
     mouseWheel();
     homeContent();
     aboutContent();
+    authorContent();
+    showMusic();
     window.onresize = fnResize;
 
+    //进度条
+    function showLoading() {
+        var oSpan = oLoading.getElementsByTagName('span')[0];
+        var aDiv = oLoading.getElementsByTagName('div');
+        //图片预加载
+        var arr = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'bg4.jpg', 'bg5.jpg', 'about1.jpg', 'about2.jpg', 'about3.jpg', 'about4.jpg', 'team.png', 'greenLine.png'];
+        var imageiNow = 0;
+
+        for (var i = 0; i < arr.length; i++) {
+            var oImage = new Image();
+            oImage.src = 'img/' + arr[i];
+            oImage.onload = function () {
+                imageiNow++;
+                oSpan.style.width = imageiNow / arr.length * 100 + '%';
+                /*if(imageiNow=arr.length){
+                 alert(1)
+                 }*/
+            };
+        }
+        oSpan.addEventListener('webKitTransitionend', spanChange, false);
+        oSpan.addEventListener('transitionend', spanChange, false);
+        function spanChange() {
+            if (imageiNow / arr.length * 100 == 100) {
+                oSpan.style.display = 'none';
+                aDiv[0].style.height = 0 + '%';
+                aDiv[1].style.height = 0 + '%';
+            }
+        };
+
+        aDiv[0].addEventListener('webKitTransitionend', divChange, false);
+        aDiv[0].addEventListener('transitionend', divChange, false);
+
+        function divChange() {
+            oLoading.parentNode.removeChild(oLoading);//进度条加载完后删除掉，提高性能
+            oMusic.onclick();
+            cjAnimate[0].inAn();
+        };
+
+        //预加载 js css
+        /*var bj = document.createElement('object');
+         bj.data = 'base.js';
+         document.body.appendChild(bj);
+         bj.width = 0;
+         bj.height = 0;
+         bj.onload = function(){
+         };*/
+
+    };
 
 
     //导航
@@ -53,16 +114,18 @@ window.onload = function () {
         for (var i = 0; i < aLiNav.length; i++) {
             aLiNav[i].index = i;
             aLiNav[i].onmousedown = function () {
-                toMove(this.index);
+                prevIndex = iNow;
                 iNow = this.index;
+                toMove(this.index);
             };
         }
         //侧边栏
         for (var i = 0; i < aLiAside.length; i++) {
-            aLiAside[i].index=i;
-            aLiAside[i].onclick=function(){
-                toMove(this.index);
+            aLiAside[i].index = i;
+            aLiAside[i].onclick = function () {
+                prevIndex = iNow;
                 iNow = this.index;
+                toMove(this.index);
             };
         }
     };
@@ -77,9 +140,18 @@ window.onload = function () {
         oList.style.top = -index * iContentHeight + 'px';
         //侧边栏
         for (var i = 0; i < aLiAside.length; i++) {
-            aLiAside[i].className='';
+            aLiAside[i].className = '';
         }
-        aLiAside[index].className='active';
+        aLiAside[index].className = 'active';
+        //入场、出场动画
+
+        if (cjAnimate) {
+            cjAnimate[index].inAn();
+        }
+        if (cjAnimate) {
+            cjAnimate[prevIndex].outAn();
+        }
+
     };
     function viewWidth() {//获取可视区的宽
         return window.innerWidth || document.documentElement.clientWidth;
@@ -154,6 +226,11 @@ window.onload = function () {
             } else {
                 bOk = ev.wheelDelta < 0 ? true : false;
             }
+
+            if (iNow == 0 && !bOk || iNow == aLiList.length - 1 && bOk) {//防止滚到第一页和最后一页继续调用出厂动画
+                return;
+            }
+            prevIndex = iNow;
             if (bOk) {//↓
                 if (iNow != aLiList.length - 1) {
                     iNow++
@@ -239,10 +316,10 @@ window.onload = function () {
             clearInterval(timer);
         };
         oHomeContent.onmouseout = function () {
-            timer = setInterval(change, 2000);
+            timer = setInterval(change, 4000);
         }
         clearInterval(timer);
-        var timer = setInterval(change, 2000);
+        var timer = setInterval(change, 4000);
 
         function change() {
             iNowHome++;
@@ -266,12 +343,12 @@ window.onload = function () {
         aBtn[i].onclick = function () {
             for (var i = 0; i < aLiMenu.length; i++) {
                 //aLiMenu[i].className = '';
-                move(aLiMenu[i],{opacity:0});
-                aLiMenu[i].style.zIndex='0';
+                move(aLiMenu[i], {opacity: 0});
+                aLiMenu[i].style.zIndex = '0';
             }
             //aLiMenu[this.index].className = 'active';
-            move(aLiMenu[this.index],{opacity:1}/*,{duration:2000}*/);
-            aLiMenu[this.index].style.zIndex='1';
+            move(aLiMenu[this.index], {opacity: 1}/*,{duration:2000}*/);
+            aLiMenu[this.index].style.zIndex = '1';
         };
     }
     //第二页（course）结束
@@ -299,7 +376,7 @@ window.onload = function () {
     function prev() {
         clearInterval(timer);
         timer = setInterval(function () {
-            left -= 5;
+            left -= 2;
             if (left <= -oWorksContent2.offsetWidth / 2) {
                 left = 0;
             }
@@ -309,7 +386,7 @@ window.onload = function () {
     function next() {
         clearInterval(timer);
         timer = setInterval(function () {
-            left += 5;
+            left += 2;
             if (left > 0) {
                 left = -oWorksContent2.offsetWidth / 2;
             }
@@ -369,6 +446,226 @@ window.onload = function () {
         }
     };
     //第四页（about/skill）结束
+
+    //第五页（author）开始
+    function authorContent() {
+        var aLi = oAuthorContent2.getElementsByTagName('li');
+        var oCanvas = null;
+        var w = 118;
+        var h = 448;
+        var timer1 = null;
+        var timer2 = null;
+        //动态创建ul和li
+        create();
+        function create() {
+            var oUl = document.createElement('ul');
+            for (var i = 0; i < 8; i++) {
+                var oLi = document.createElement('li');
+                oLi.style.backgroundPosition = -i * w + 'px,0';
+                oUl.appendChild(oLi);
+            }
+            oAuthorContent2.appendChild(oUl);
+        };
+        //绑定li动态创建canvas
+        bindList();
+        function bindList() {
+            oAuthorContent2.onmouseleave = function () {
+                removeCanvas();
+            }
+            for (var i = 0; i < aLi.length; i++) {
+                aLi[i].index = i;
+                aLi[i].onmouseover = function () {
+                    addCanvas();
+                    oCanvas.style.left = this.index * w + 'px'
+                };
+            }
+        };
+        function addCanvas() {
+            if (!oCanvas) {
+                oCanvas = document.createElement('canvas');
+                oCanvas.id = 'canvasBubble';
+                oCanvas.width = w;
+                oCanvas.height = h;
+                oAuthorContent2.appendChild(oCanvas);
+                bindCanvas();
+            }
+        }
+
+        function removeCanvas() {
+            clearInterval(timer1);
+            clearInterval(timer2);
+            oAuthorContent2.removeChild(oCanvas);
+            oCanvas = null;
+        };
+        function bindCanvas() {
+            var ctx = oCanvas.getContext('2d');
+
+            var setArr = [];//存储要绘制的所有图形的数据
+
+            timer1 = setInterval(function () {
+                //console.log(setArr.length);
+                ctx.clearRect(0, 0, oCanvas.width, oCanvas.height);//清除画布路径
+                for (var i = 0; i < setArr.length; i++) {//圆在参数不断变化
+                    setArr[i].num += 5;
+                    //曲线运动
+                    setArr[i].x = setArr[i].startX - Math.sin(setArr[i].num * Math.PI / 180) * setArr[i].step;
+                    setArr[i].y = setArr[i].startY - (setArr[i].num * Math.PI / 180) * setArr[i].step;
+                    if (setArr[i].y < 50) {
+                        setArr.splice(i, 1);//y坐标小于50就删除，提高性能
+                    }
+                }
+
+                for (var i = 0; i < setArr.length; i++) {//绘制圆
+                    ctx.fillStyle = 'rgba(' + setArr[i].c1 + ',' + setArr[i].c2 + ',' + setArr[i].c3 + ',' + setArr[i].c4 + ')';
+                    ctx.beginPath();//开启一个新的路径
+                    ctx.moveTo(setArr[i].x, setArr[i].y);//起点
+                    ctx.arc(setArr[i].x, setArr[i].y, setArr[i].r, 0, 360 * Math.PI / 180, false);
+                    ctx.closePath();//自动闭合路径,不加上会出现缺角的效果 加在上色之前
+                    ctx.fill();
+                }
+            }, 16);
+            timer2 = setInterval(function () {//开定时器存储圆的信息
+                var x = Math.random() * oCanvas.width;
+                var y = oCanvas.height - 10;
+                var r = Math.random() * 6 + 2;
+                //随机颜色
+                var c1 = Math.round(Math.random() * 255);
+                var c2 = Math.round(Math.random() * 255);
+                var c3 = Math.round(Math.random() * 255);
+                var c4 = 1;
+
+                var num = 0;//初始角度值
+                var step = Math.random() * 20 + 10;//放大系数(速度和波峰波谷)
+                //起始值
+                var startX = x;
+                var startY = y;
+
+                setArr.push({
+                    x: x,
+                    y: y,
+                    r: r,
+                    c1: c1,
+                    c2: c2,
+                    c3: c3,
+                    c4: c4,
+                    num: num,
+                    step: step,
+                    startX: x,
+                    startY: y
+                });
+                //console.log(setArr)
+            }, 100);
+        };
+    };
+
+    //第五页（author）结束
+
+    //入场/出场动画 开始
+
+    var cjAnimate = [
+        {
+            inAn: function () {
+                oHomeContent1.style.opacity = 1;
+                oHomeContent2.style.opacity = 1;
+                setStyle(oHomeContent1, 'transform', 'translate(0,0)');
+                setStyle(oHomeContent2, 'transform', 'translate(0,0)');
+            },
+            outAn: function () {
+                oHomeContent1.style.opacity = 0;
+                oHomeContent2.style.opacity = 0;
+                setStyle(oHomeContent1, 'transform', 'translate(0,-150px)');
+                setStyle(oHomeContent2, 'transform', 'translate(0,100px)');
+            }
+        },
+        {
+            inAn: function () {
+                var oPlane1 = getByClass(oCourseContent, 'plane1')[0];
+                var oPlane2 = getByClass(oCourseContent, 'plane2')[0];
+                var oPlane3 = getByClass(oCourseContent, 'plane3')[0];
+                setStyle(oPlane1, 'transform', 'translate(0,0)');
+                setStyle(oPlane2, 'transform', 'translate(0,0)');
+                setStyle(oPlane3, 'transform', 'translate(0,0)');
+            },
+            outAn: function () {
+                var oPlane1 = getByClass(oCourseContent, 'plane1')[0];
+                var oPlane2 = getByClass(oCourseContent, 'plane2')[0];
+                var oPlane3 = getByClass(oCourseContent, 'plane3')[0];
+                setStyle(oPlane1, 'transform', 'translate(-200px,-200px)');
+                setStyle(oPlane2, 'transform', 'translate(-200px,200px)');
+                setStyle(oPlane3, 'transform', 'translate(200px,-200px)');
+            }
+        },
+        {
+            inAn: function () {
+                var oPencel1 = getByClass(oWorksContent, 'pencel1')[0];
+                var oPencel2 = getByClass(oWorksContent, 'pencel2')[0];
+                var oPencel3 = getByClass(oWorksContent, 'pencel3')[0];
+                setStyle(oPencel1, 'transform', 'translate(0,0)');
+                setStyle(oPencel2, 'transform', 'translate(0,0)');
+                setStyle(oPencel3, 'transform', 'translate(0,0)');
+            },
+            outAn: function () {
+                var oPencel1 = getByClass(oWorksContent, 'pencel1')[0];
+                var oPencel2 = getByClass(oWorksContent, 'pencel2')[0];
+                var oPencel3 = getByClass(oWorksContent, 'pencel3')[0];
+                setStyle(oPencel1, 'transform', 'translate(0,-200px)');
+                setStyle(oPencel2, 'transform', 'translate(0,200px)');
+                setStyle(oPencel3, 'transform', 'translate(0,200px)');
+            }
+        },
+        {
+            inAn: function () {
+                var aAboutImg = getByClass(oAboutContent, 'aboutImg');
+                setStyle(aAboutImg[0], 'transform', 'rotate(0)');
+                setStyle(aAboutImg[1], 'transform', 'rotate(0)');
+            },
+            outAn: function () {
+                var aAboutImg = getByClass(oAboutContent, 'aboutImg');
+                setStyle(aAboutImg[0], 'transform', 'rotate(45deg)');
+                setStyle(aAboutImg[1], 'transform', 'rotate(-45deg)');
+            }
+        },
+        {
+            inAn: function () {
+                oAuthorContent1.style.opacity = 1;
+                setStyle(oAuthorContent1, 'transform', 'translate(0,0)');
+                setStyle(oAuthorContent2, 'transform', 'translate(0,0)');
+            },
+            outAn: function () {
+                oAuthorContent1.style.opacity = 0;
+                setStyle(oAuthorContent1, 'transform', 'translate(-200px,0)');
+                setStyle(oAuthorContent2, 'transform', 'translate(0,800px)');
+            }
+        }
+    ];
+    //cjAnimate[4].outAn();
+    //setTimeout(function () {
+    //    cjAnimate[0].inAn();
+    //}, 1000);
+
+    for (var i = 0; i < cjAnimate.length; i++) {
+        cjAnimate[i].outAn();
+    }
+    /*setTimeout(function () {
+     cjAnimate[0].inAn();
+     }, 1000);
+     */
+    //入场/出场动画 结束
+
+    //音乐
+    function showMusic() {
+        var onoff = true;
+        oMusic.onclick = function () {
+            if (onoff) {
+                this.style.background = 'url(img/musicon.gif)';
+                oAudio1.play();
+            } else {
+                this.style.background = 'url(img/musicoff.gif)';
+                oAudio1.pause();
+            }
+            onoff = !onoff;
+        };
+    };
 
     function ran(n, m) {
         return Math.round(Math.random() * (m - n) + n);
